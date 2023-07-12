d1907542201232d9d79232ae7180376bd86d14bc
mbsagin <mbsagin@gmail.com>
2023-07-12 18:02:32 +0300

    SeriLog as Logger with configs

--- Elastic.Kibana.Serilog/Program.cs
+++ Elastic.Kibana.Serilog/Program.cs
@@ -1,4 +1,20 @@
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add Logging
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+builder.Configuration.AddJsonFile(
+                $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+                optional: true);
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog();
+
+// Configure Logging
+ConfigureLogging();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -21,3 +37,36 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+static void ConfigureLogging()
+{
+    var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+    var configuration = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile(
+            $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+            optional: true)
+        .Build();
+
+    var elasticOptions = ConfigureElasticSink(configuration, environment);
+
+    Log.Logger = new LoggerConfiguration()
+        .Enrich.FromLogContext()
+        .Enrich.WithMachineName()
+        .WriteTo.Debug()
+        .WriteTo.Console()
+        .WriteTo.Elasticsearch(elasticOptions)
+        .Enrich.WithProperty("Environment", environment)
+        .ReadFrom.Configuration(configuration)
+        .CreateLogger();
+}
+
+static ElasticsearchSinkOptions ConfigureElasticSink(IConfigurationRoot configuration, string environment)
+{
+    return new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
+    {
+        AutoRegisterTemplate = true,
+        IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+    };
+}
